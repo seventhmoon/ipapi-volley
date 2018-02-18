1f787dcf7e8d048bfc12b96ba81a42eb2b1b7320
Fung LAM <seventhmoon@gmail.com>
2018-02-18 15:26:44 +0800

    clean up

--- app/src/main/java/com/androidfung/geoip/MainActivity.java
+++ app/src/main/java/com/androidfung/geoip/MainActivity.java
@@ -4,17 +4,12 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.Volley;
 import com.androidfung.geoip.api.ApiManager;
 import com.androidfung.geoip.databinding.ActivityMainBinding;
-import com.androidfung.geoip.model.GeoIpResponseModel;
 
 
 
@@ -30,23 +25,11 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         ApiManager apiManager = new ApiManager(Volley.newRequestQueue(this));
-        apiManager.getGeoIpInfo(binding::setResponse, error -> Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show());
+        apiManager.getGeoIpInfo(binding::setResponse, this::showError);
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-
-        return super.onOptionsItemSelected(item);
+    private void showError(VolleyError error){
+        Toast.makeText(this, error.toString(), Toast.LENGTH_SHORT).show();
     }
 }
